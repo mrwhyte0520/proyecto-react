96df3658a697891282588bd1fd37b440488e5770
mrwhyte0520 <fw2023-3959@uce.edu.do>
2026-06-04 19:58:52 -0400

    cambios

--- src/api/pokeinfo/index.ts
+++ src/api/pokeinfo/index.ts
@@ -1,11 +1,12 @@
-export function getpokemon(){
-    return fetch('https://pokeapi.co/api/v2/pokemon/')
-    .then(response => response.json())
-    .then(data =>  data.results);
+export function getpokemon(url = 'https://pokeapi.co/api/v2/pokemon/'){
+     return fetch(url)
+    .then(response => response.json());
+    
 }
 
 export function getpokemondetalle(url:string){
     return fetch(url)
     .then(response => response.json())
+    
    
 } 
--- src/assets/Components/pokemones.tsx
+++ src/assets/Components/pokemones.tsx
@@ -1,33 +1,93 @@
 import { useState,useEffect } from "react";
 import type {   pokemon,  pokemonsdetalle } from "../../api/pokeinfo/Models";
 import {  getpokemon,getpokemondetalle } from "../../api/pokeinfo";
+import {Card, Pagination, Col, Row} from 'antd';
 
+
+ 
 function Pokemones(){
   const [pokemons, setPokemons] = useState<pokemonsdetalle[]>([]);
-
+  const [nextUrl, setNextUrl] = useState<string>("");
+  const [previousUrl, setPreviousUrl] = useState<string>("");
+  const [count, setCount] = useState(0);
   useEffect(() => {
-    getpokemon().then(async (lista: pokemon[]) => {
-      const detalles = await Promise.all(
-        lista.map(pokemon => getpokemondetalle(pokemon.url))
-      );
+    getpokemon().then(data => {
+        setNextUrl(data.next);
+        setPreviousUrl(data.previous);
+        setCount(data.count);
 
-      setPokemons(detalles);
-     
+        Promise.all(
+            data.results.map((pokemon: pokemon) =>
+                getpokemondetalle(pokemon.url)
+            )
+        ).then(detalles => {
+            setPokemons(detalles);
+        });
     });
-  }, []);
+}, []);
    console.log(pokemons);
  return <div>
     <h1> Mis Pokemones</h1>
-    <ul>
-        {pokemons.map (pokemon  => <li key={pokemon.id}>
-             
-            <h2>{pokemon.name}</h2>
-            <img src ={pokemon.sprites.front_default} alt="" />
-            
-            
 
-        </li>)}
-    </ul>
+    <form>  
+        <input type="text" placeholder="Buscar pokemon" />
+        <button type="submit">Buscar</button>
+    </form>
+    <button className="BotonAtras"  disabled={!previousUrl} onClick={() => {
+        getpokemon(previousUrl).then(data => {
+            setNextUrl(data.next);
+            setPreviousUrl(data.previous);
+           
+
+            Promise.all(
+                data.results.map((pokemon: pokemon) =>
+                    getpokemondetalle(pokemon.url)
+                )
+            ).then(detalles => {
+                setPokemons(detalles);
+            });
+        });
+    }}>
+        Anteriores
+    </button>
+    <button className="BotonSiguiente" disabled={!nextUrl} onClick={() => {
+        getpokemon(nextUrl).then(data => {
+            setNextUrl(data.next);
+            setPreviousUrl(data.previous);
+            setCount(data.count);
+
+            Promise.all(
+                data.results.map((pokemon: pokemon) =>
+                    getpokemondetalle(pokemon.url)
+                )
+            ).then(detalles => {
+                setPokemons(detalles);
+            });
+        });
+    }}
+
+    >Siguientes</button>
+
+   <Row gutter={[16, 16]}>
+
+   
+   
+    {pokemons.map(pokemon => (
+            
+    <Col key={pokemon.id} span={4}>
+       <Card title={`${pokemon.id} ${pokemon.name}` }style={{ width: 200 ,height: 200}   }>
+         {pokemon.sprites?.front_default && (
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+       )}
+      </Card>
+   </Col>
+         ))}
+</Row>
+
+
+   
+
+
  </div> 
 
 }
